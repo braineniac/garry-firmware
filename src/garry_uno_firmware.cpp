3dce61b4fff49b240970ef85faf7e6eb954a1fe0
Daniel Hammer <braineniac@protonmail.com>
2019-01-23 11:22:32 +0100

    added cmd_vel

--- src/garry_uno_firmware.cpp
+++ src/garry_uno_firmware.cpp
@@ -4,6 +4,7 @@
 //#define ENABLE_LOG
 
 #define MAX_LOG_LEN 30
+#define MOTOR_DELAY 100
 
 /* global variables */
 Servo servoA;
@@ -14,9 +15,7 @@ ros::NodeHandle nh;
 ros::Subscriber<std_msgs::Bool> sub_led_right_eye("led_right_eye", &led_right_eye_cb);
 ros::Subscriber<std_msgs::Bool> sub_led_left_eye("led_left_eye", &led_left_eye_cb);
 
-ros::Subscriber<geometry_msgs::Twist> sub_motor_left("motor_left", &motor_left_cb);
-ros::Subscriber<geometry_msgs::Twist> sub_motor_right("motor_right", &motor_right_cb);
-ros::Subscriber<geometry_msgs::Twist> sub_motor_both("motor_both", &motor_both_cb);
+ros::Subscriber<geometry_msgs::Twist> sub_cmd_vel("cmd_vel", &cmd_vel_cb);
 
 ros::Subscriber<geometry_msgs::Twist> sub_servo_upper("servo_upper", &servo_upper_cb);
 ros::Subscriber<geometry_msgs::Twist> sub_servo_lower("servo_lower", &servo_lower_cb);
@@ -46,11 +45,9 @@ void setup() {
     nh.initNode();
     nh.subscribe(sub_led_left_eye);
     nh.subscribe(sub_led_right_eye);
-    nh.subscribe(sub_motor_left);
-    nh.subscribe(sub_motor_right);
     nh.subscribe(sub_servo_upper);
     nh.subscribe(sub_servo_lower);
-    nh.subscribe(sub_motor_both);
+    nh.subscribe(sub_cmd_vel);
 }
 
 /* main loop */
@@ -90,86 +87,16 @@ void led_right_eye_cb(const std_msgs::Bool& msg) {
     #endif
 }
 
-void motor_right_cb(const geometry_msgs::Twist& msg) {
+void cmd_vel_cb(const geometry_msgs::Twist& msg) {
 
-    uint8_t speed = (uint8_t) (255 * msg.linear.x);
-
-    #ifdef ENABLE_LOG
-    char logoutput[MAX_LOG_LEN];
-    snprintf(logoutput,MAX_LOG_LEN,"Left motor moving, speed: %d",speed);
-    nh.loginfo(logoutput);
-    #endif
-
-    if (msg.linear.x > 0)
-        digitalWrite(12, LOW); // A motor forward
-
-    else
-        digitalWrite(12, HIGH); // A motor backwards
-
-
-    digitalWrite(9, LOW); // disable A motor brake
-    analogWrite(3, speed);
-
-    delay(1000);
-
-    digitalWrite(9, HIGH); //enable motor A brake
-}
-
-void motor_left_cb(const geometry_msgs::Twist& msg) {
-
-    uint8_t speed = (uint8_t) (255 * abs(msg.linear.x));
-
-    #ifdef ENABLE_LOG
-    char logoutput[MAX_LOG_LEN];
-    snprintf(logoutput,MAX_LOG_LEN,"Right motor moving, speed: %d",speed);
-    nh.loginfo(logoutput);
-    #endif
-
-    if (msg.linear.x > 0)
-        digitalWrite(13, HIGH); // B motor forward
-
-    else
-        digitalWrite(13, LOW); // B motor backwards
-
-    digitalWrite(8, LOW); // disable B motor brake
-    analogWrite(11, speed);
-
-    delay(1000);
-
-    digitalWrite(8, HIGH); // enable motor B brake
-
-}
-
-void motor_both_cb(const geometry_msgs::Twist& msg) {
-
-    uint8_t speed = (uint8_t) (255 * abs(msg.linear.x));
-
-    #ifdef ENABLE_LOG
-    char logoutput[MAX_LOG_LEN];
-    snprintf(logoutput,MAX_LOG_LEN,"Both motors moving, speed: %d",speed);
-    nh.loginfo(logoutput);
-    #endif
-
-    if (msg.linear.x > 0) {
-        digitalWrite(13, HIGH); // B motor forward
-        digitalWrite(12, LOW); // A motor forward
+    if(msg.linear.x != 0) {
+        uint8_t speed = (uint8_t) (255 * msg.linear.x);
+        (msg.linear.x > 0) ? forward(speed) : backward(speed);
     }
-    else {
-        digitalWrite(13, LOW); // B motor backwards
-        digitalWrite(12, HIGH); // A motor forward
+    else if (msg.angular.z != 0) {
+        uint8_t speed = (uint8_t) (255 * msg.angular.z);
+        (msg.angular.z > 0) ? turn_left(speed) : turn_right(speed);
     }
-
-
-    digitalWrite(9, LOW); // disable A motor brake
-    digitalWrite(8,LOW); // disable B motor brake
-
-    analogWrite(11, speed);
-    analogWrite(3,speed);
-
-    delay(1000);
-
-    digitalWrite(9, HIGH); // enable motor A brake
-    digitalWrite(8, HIGH); // enable motor B brake
 }
 
 void servo_upper_cb(const geometry_msgs::Twist& msg) {
@@ -183,7 +110,6 @@ void servo_upper_cb(const geometry_msgs::Twist& msg) {
     #endif
 
     servoA.write(angle);
-
 }
 
 void servo_lower_cb(const geometry_msgs::Twist& msg) {
@@ -197,5 +123,68 @@ void servo_lower_cb(const geometry_msgs::Twist& msg) {
     #endif
 
     servoB.write(angle);
+}
+/* other functions */
 
+void run_motors(const double speed) {
+
+    digitalWrite(8, LOW); // disable B motor brake
+    digitalWrite(9, LOW); // disable A motor brake
+    analogWrite(3, speed);
+    analogWrite(11, speed);
+
+    delay(MOTOR_DELAY);
+
+    digitalWrite(9, HIGH); //enable motor A brake
+    digitalWrite(8, HIGH); // enable motor B brake
+}
+
+void turn_left(const double speed) {
+
+    #ifdef ENABLE_LOG
+    char logoutput[MAX_LOG_LEN];
+    snprintf(logoutput,MAX_LOG_LEN,"Left motor moving, speed: %d",speed);
+    nh.loginfo(logoutput);
+    #endif
+
+    digitalWrite(13, LOW); // B motor backwards
+    digitalWrite(12, HIGH); // A motor forward
+
+    run_motors(speed);
+}
+
+void turn_right(const double speed) {
+
+    #ifdef ENABLE_LOG
+    char logoutput[MAX_LOG_LEN];
+    snprintf(logoutput,MAX_LOG_LEN,"Right motor moving, speed: %d",speed);
+    nh.loginfo(logoutput);
+    #endif
+
+    digitalWrite(13, HIGH); // B motor forward
+    digitalWrite(12, LOW); // A motor backward
+
+    run_motors(speed);
+}
+
+void forward(const double speed) {
+
+    #ifdef ENABLE_LOG
+    char logoutput[MAX_LOG_LEN];
+    snprintf(logoutput,MAX_LOG_LEN,"Both motors moving, speed: %d",speed);
+    nh.loginfo(logoutput);
+    #endif
+
+    digitalWrite(13, HIGH); // B motor forward
+    digitalWrite(12, HIGH); // A motor forward
+
+    run_motors(speed);
+}
+
+void backward(const double speed) {
+
+    digitalWrite(13, LOW); // B motor backwards
+    digitalWrite(12, LOW); // A motor forward
+
+    run_motors(speed);
 }
--- src/garry_uno_firmware.h
+++ src/garry_uno_firmware.h
@@ -11,7 +11,6 @@
 #include <std_msgs/Bool.h>
 #include <geometry_msgs/Twist.h>
 
-
 /* function definitions */
 void led_left_eye_cb(const std_msgs::Bool& msg);
 void led_right_eye_cb(const std_msgs::Bool& msg);
@@ -19,6 +18,9 @@ void led_right_eye_cb(const std_msgs::Bool& msg);
 void servo_upper_cb(const geometry_msgs::Twist& msg);
 void servo_lower_cb(const geometry_msgs::Twist& msg);
 
-void motor_left_cb(const geometry_msgs::Twist& msg);
-void motor_right_cb(const geometry_msgs::Twist& msg);
-void motor_both_cb(const geometry_msgs::Twist& msg);
+void cmd_vel_cb(const geometry_msgs::Twist& msg);
+void turn_right(const double speed);
+void turn_left(const double speed);
+void forward(const double speed);
+void backward(const double speed);
+void run_motors(const double speed);
